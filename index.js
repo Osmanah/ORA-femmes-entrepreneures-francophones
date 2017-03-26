ddaab783411e17892c439e88a58b40b567d25955
osman <osman.ahmady@gmail.com>
2017-03-26 13:59:35 +0200

    initialiser le code pour la plateforme ORA  pendant Hachathon Inovathon entre 22 et 24 mars

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// var imag = document.getElementById('image1');
+// var imag = document.getElementById('image2');
+// var imag = document.getElementById('image3');
+//
+//
+//
+// var myIndex = 0;
+// carousel();
+//
+// function carousel() {
+//     var x = document.getElementsByClassName("mySlides");
+//     for (var i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+//     myIndex++;
+//     if (myIndex > x.length) {
+//         myIndex = 1
+//     }
+//     x[myIndex - 1].style.display = "block";
+//     setTimeout(carousel, 10000000);
+// }
+//
+// var a = 1+2;
+// console.log(a);
